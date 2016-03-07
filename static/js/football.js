3d466de3acce9d4ac4f32b43700988cf20e4c479
DarKnight <05.DarKnight.11@gmail.com>
2016-03-07 10:55:59 +0530

    Added football page

---
+++ static/js/football.js
@@ -0,0 +1,16 @@
+//$("#league_points_table").click(function(){
+//    $("#league_table").show();
+//});
+
+$('#league_points_table').click(function() {
+    $('#league_table').slideToggle();
+});
+
+$('#top_scorers').click(function() {
+    $('#goalscorer').slideToggle();
+});
+
+$('#match_results').click(function() {
+    $('#results').slideToggle();
+});
+
